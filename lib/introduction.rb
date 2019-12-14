d8fb78f8fa617bec02e80f020c2317797ef9fa80
Olivia Madden <madden.livi@gmail.com>
2019-12-14 18:45:10 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
-def indroduction_with_language(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def indroduction_with_language_optional(name, language='Ruby')
+def introduction_with_language_optional(name, language='Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
